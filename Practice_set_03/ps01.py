d46d6051fe4193fa7398ebf65bbec4238d7377d2
Pranta-Nag <prantanag10@gmail.com>
2025-07-23 12:36:22 +0600

    another

---
+++ Practice_set_03/ps01.py
@@ -0,0 +1,36 @@
+##### Write a python program to display a user entered name followed by Good afternoon using input() function.
+
+name = input("Enter your name: ")
+print(f"Good afternoon {name}")             # using f string
+
+
+
+"""
+Write a program to fill in a letter template given below with name and date
+    letter = '''
+                Dear <name>,
+                You are selected!
+                <date>    
+    '''
+"""
+
+letter = '''
+ Dear <name>,
+ You are selected!
+ <date>    
+    '''
+name = input("Enter a name:")
+date = input("Enter date:")
+
+print(letter.replace("<name>", name).replace("<date>", date))
+
+
+
+##### Write a program to detect double space in a string
+
+text = "I am a good boy."
+
+print(text.find("  "))              # find double space
+
+
+
---
+++ String/string_functions.py
@@ -0,0 +1,34 @@
+name = "pranta nag"
+
+print(len(name))            # print length
+
+print(name.endswith("ta"))                  # return false. Because last two word is ta.
+
+print(name.startswith("pr"))                  # return True. Because first two word is pr.
+
+print(name.capitalize())                    # capital first character
+
+print(name.upper())                         # Converts all characters to uppercase
+
+print(name.lower())                          # Converts all characters to lowercase
+
+print(name.isalpha())                       # Checks if all characters are alphabets
+
+print(name.isdigit())                       # Checks if all characters are digits
+
+print(name.split())                         # Splits string into a list
+
+print(name.find("t"))                       # Returns the first index of a substring or -1
+
+print(name.rstrip())                        # Removes trailing whitespaces
+
+print(name.replace("pranta", "Joy"))        # Replaces all occurrences of a substring
+
+
+
+
+
+
+
+
+
